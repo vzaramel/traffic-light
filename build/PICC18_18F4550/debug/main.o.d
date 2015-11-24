a10ee92fd569307b5a2adb2caf97a1a9defc4138
Vinicius Zaramella <vinicius.zaramella@gmail.com>
2015-11-23 17:08:18 -0800

    removido arquivos de build

--- build/PICC18_18F4550/debug/main.o.d
+++
@@ -1,4 +0,0 @@
-build/PICC18_18F4550/debug/main.o:  \
-	C:/Users/IEUser/MPLABXProjects/trafficligth.X/main.c  \
-	C:/Users/IEUser/MPLABXProjects/trafficligth.X/traffic_light_control.h  \
-
--- build/PICC18_18F4550/production/traffic_light_control.o.d
+++
@@ -1,4 +0,0 @@
-build/PICC18_18F4550/production/traffic_light_control.o:  \
-	C:/Users/IEUser/MPLABXProjects/trafficligth.X/traffic_light_control.c  \
-	C:/Users/IEUser/MPLABXProjects/trafficligth.X/traffic_light_control.h  \
-
